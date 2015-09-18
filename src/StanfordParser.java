9ad0000bd8bb4f20da38cf40ccea9aa432bb25cb
unknown <yawenluo@DC3SP05.adsroot.itcs.umich.edu>
2015-09-17 22:42:49 -0400

    add index in tree

--- src/StanfordParser.java
+++ src/StanfordParser.java
@@ -1,23 +1,35 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Properties;
+import java.util.Set;
+import java.util.Vector;
 
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
-class PotentialTrashNode {
+/*class PotentialTrashNode {
 	public Tree Node;
 	public int index;
 	PotentialTrashNode(Tree Node, int index) {
 		this.Node = Node.deepCopy();
 		this.index = index;
 	}
-}
-
+}*/
+/*class Word {
+	public int index;
+	public boolean isStart;
+	Word(int i, boolean start) {
+		this.index = i;
+		this.isStart = start;
+	}
+}*/
 public class StanfordParser {
 
 	public static void main(String[] args) {
@@ -26,7 +38,8 @@ public class StanfordParser {
 	}
 
 	private StanfordCoreNLP pipeline;
-	
+	int index_counter = 0;
+	List<Integer> start_word_records;
 	public StanfordParser() {
 		Properties parserProperties = new Properties();
 		parserProperties.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
@@ -79,6 +92,7 @@ public class StanfordParser {
 	private void PrintSentence(Tree clauseTree) {
 		StringBuilder TempClause = new StringBuilder();
 		boolean LoopStart = false;
+		boolean findFirstWord = false;
 		for (Tree clause : clauseTree) {
 			if(clause.nodeString().contains("SBAR") && !clause.isLeaf() && LoopStart) {
 				break;
@@ -87,9 +101,45 @@ public class StanfordParser {
 				LoopStart = true;
 			}
 			if(clause.isLeaf()) {
+				index_counter++;
+				if(!findFirstWord) {
+					start_word_records.add(index_counter);
+					findFirstWord = true;
+				}
+				
+			}
+			if(clause.isLeaf()) {
 				TempClause.append(clause.nodeString() + " ");
 			}
 		}
 		System.out.println(TempClause.toString());
 	}
+	
+	@SuppressWarnings("null")
+	public void Test(String text) {
+		Annotation annotation = new Annotation(text); 
+		pipeline.annotate(annotation);
+		List<CoreMap> sentences = annotation.get(SentencesAnnotation.class); //list of sentences in the document
+		for (CoreMap sentence : sentences) {
+			Tree constituencyTree = sentence.get(TreeAnnotation.class);  //constituency tree of a sentence
+			ArrayList<Tree> ClauseList = CutSentence(constituencyTree); 
+			SemanticGraph semanticGraph = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
+			Set<IndexedWord> vertices = semanticGraph.vertexSet();
+			for(Tree clause : ClauseList) {
+				PrintSentence(clause);
+			}
+			List<IndexedWord> start_word_vertices = null;
+			for(Integer index : start_word_records) {
+				for(IndexedWord node : vertices) {
+					if(node.index() == index) {
+						start_word_vertices.add(node);
+					}
+				}
+			}
+/*			for(Tree clause : ClauseList) {
+				PrintSentence(clause);
+			}*/
+		}
+	}
+	
 }
