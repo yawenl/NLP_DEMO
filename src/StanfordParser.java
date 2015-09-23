e34d81c89d90b0a1048d88eea1677003237c98e7
qingzhouhe <qingzhou@umich.edu>
2015-09-22 20:55:14 -0400

    merge with yawen

--- src/StanfordParser.java
+++ src/StanfordParser.java
@@ -1,3 +1,8 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.Set;
+
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.pipeline.Annotation;
@@ -8,30 +13,28 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.Set;
-
-/*class PotentialTrashNode {
+class PotentialTrashNode {
 	public Tree Node;
 	public int index;
 	PotentialTrashNode(Tree Node, int index) {
 		this.Node = Node.deepCopy();
 		this.index = index;
 	}
-}*/
+}
 
 public class StanfordParser {
 
 	public static void main(String[] args) {
 		StanfordParser parser = new StanfordParser();
-		parser.Test("ABC cites the fact that chemical additives are banned in many countries and feels they may be banned in this state too");
+		String text = "ABC cites the fact that chemical additives are banned in many countries and feels they may be banned in this state too";
+		parser.parse(text);
+		parser.Test(text);
 	}
 
 	private StanfordCoreNLP pipeline;
 	int index_counter = 0;
 	List<Integer> start_word_records = new ArrayList<Integer>();
+	
 	public StanfordParser() {
 		Properties parserProperties = new Properties();
 		parserProperties.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
@@ -53,43 +56,61 @@ public class StanfordParser {
 		for (CoreMap sentence : sentences) {
 			Tree constituencyTree = sentence.get(TreeAnnotation.class);  //constituency tree of a sentence
 			ArrayList<Tree> ClauseList = CutSentence(constituencyTree); 
-			
-			for(Tree clause : ClauseList) {
-				PrintSentence(clause);
-			}
+			PrintSentences(ClauseList);
 		}
 	}
+	
+	private String getLabelString(Tree node) {
+		String nodeString = node.nodeString();
+		String labelString = "";
+		for (int i = 0; i < nodeString.length(); i++){
+		    char c = nodeString.charAt(i);        
+		    if(c >= 'A' && c <= 'Z') {
+		    	labelString += c;
+		    }
+		}
+		return labelString;
+	}
+	
+	private boolean isClauseStart(Tree node) {
+		boolean isSBAR = getLabelString(node).equals("SBAR");
+		boolean isS = getLabelString(node).equals("S");
+		boolean isSBARQ = getLabelString(node).equals("SBARQ");
+		boolean isSINV = getLabelString(node).equals("SINV");
+		boolean isSQ = getLabelString(node).equals("SQ");
+		return isSBAR || isS || isSBARQ || isSINV || isSQ;
+	}
+	
+//  Sample code to remove child node from parent node
+//	Tree parent = clause.parent(constituencyTree);
+//	if(parent != null) {
+//		parent.removeChild(parent.objectIndexOf(clause));
+//	}	
 	
 	private ArrayList<Tree> CutSentence(Tree constituencyTree) {
 		ArrayList<Tree> ClauseList = new ArrayList<Tree>();
 		for (Tree clause : constituencyTree) {
-			if(ClauseList.size() == 0) {
-				ClauseList.add(clause.deepCopy());
-			}
-			if(clause.nodeString().contains("SBAR") && !clause.isLeaf()) {
-				if (clause.firstChild().nodeString().contains("IN")) {
+			if(isClauseStart(clause) && !clause.isLeaf()) {
+				if (getLabelString(clause.firstChild()).equals("IN")) {
 					clause.removeChild(0);
 				}
-				ClauseList.add(clause.deepCopy());
 				Tree parent = clause.parent(constituencyTree);
 				if(parent != null) {
-					parent.removeChild(parent.objectIndexOf(clause));
-				}		
+					if(!isClauseStart(parent)) {
+						ClauseList.add(clause);
+					}
+				}	
 			}
 		}
 		return ClauseList;
 	}
 	
-	private void PrintSentence(Tree clauseTree) {
+	private void PrintSentence(Tree clauseTree, Tree nextClause) {
 		StringBuilder TempClause = new StringBuilder();
-		boolean LoopStart = false;
 		boolean findFirstWord = false;
 		for (Tree clause : clauseTree) {
-			if(clause.nodeString().contains("SBAR") && !clause.isLeaf() && LoopStart) {
+			if(clause.equals(nextClause)) {
 				break;
-			}
-			if(!LoopStart) {
-				LoopStart = true;
 			}
 			if(clause.isLeaf()) {
 				index_counter++;
@@ -97,16 +118,24 @@ public class StanfordParser {
 					start_word_records.add(index_counter);
 					findFirstWord = true;
 				}
-				
-			}
-			if(clause.isLeaf()) {
 				TempClause.append(clause.nodeString() + " ");
 			}
 		}
 		System.out.println(TempClause.toString());
 	}
 	
-	@SuppressWarnings("null")
+	private void PrintSentences(ArrayList<Tree> ClauseList) {
+		Tree nextClause = null;
+		for(int i = 0; i < ClauseList.size(); i ++) {
+			if(i+1 < ClauseList.size()) {
+				nextClause = ClauseList.get(i+1);
+			} else {
+				nextClause = null;
+			}
+			PrintSentence(ClauseList.get(i), nextClause);
+		}
+	}
+	
 	public void Test(String text) {
 		Annotation annotation = new Annotation(text); 
 		pipeline.annotate(annotation);
@@ -116,32 +145,28 @@ public class StanfordParser {
 			ArrayList<Tree> ClauseList = CutSentence(constituencyTree); 
 			SemanticGraph semanticGraph = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
 			Set<IndexedWord> vertices = semanticGraph.vertexSet();
-			for(Tree clause : ClauseList) {
-				PrintSentence(clause);
-			}
-			List<IndexedWord> start_word_vertices = ArrayList<Integer>();
+			PrintSentences(ClauseList);
+			List<IndexedWord> start_word_vertices = new ArrayList<IndexedWord>();
 			for(Integer index : start_word_records) {
 				for(IndexedWord node : vertices) {
+					System.out.println("node.index() = " + node.index());
+					System.out.println("node = " + node.toString());
+					System.out.println("index = " + index);
 					if(node.index() == index) {
 						start_word_vertices.add(node);
 					}
 				}
 			}
-			ArrayList<Set<IndexedWord>> clauses_of_indexedwords = null;
-			for(IndexedWord index : start_word_vertices) {
-				clauses_of_indexedwords.add(semanticGraph.getChildren(index));
-			}
-			
-			for(Set<IndexedWord> temp_set : clauses_of_indexedwords) {
-				for(IndexedWord index : temp_set) {
-					System.out.println(index.word());
-				}
-			}
-			
-/*			for(Tree clause : ClauseList) {
-				PrintSentence(clause);
-			}*/
+//			ArrayList<Set<IndexedWord>> clauses_of_indexedwords = null;
+//			for(IndexedWord index : start_word_vertices) {
+//				clauses_of_indexedwords.add(semanticGraph.getChildren(index));
+//			}
+//			
+//			for(Set<IndexedWord> temp_set : clauses_of_indexedwords) {
+//				for(IndexedWord index : temp_set) {
+//					System.out.println(index.word());
+//				}
+//			}
 		}
 	}
-	
 }
