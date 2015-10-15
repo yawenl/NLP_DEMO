e4dd0900a42f7f214cc1fd7e55780c74996dbc26
qingzhouhe <qingzhou@umich.edu>
2015-10-15 19:38:46 -0400

    Update Clause.java

--- src/Clause.java
+++ src/Clause.java
@@ -1,15 +1,67 @@
 import java.util.ArrayList;
 import edu.stanford.nlp.ling.IndexedWord;
 
+class subNerClause {
+	public int start;
+	public int end;
+	public String ner;
+	subNerClause() {
+		start = -1;
+		end = -1;
+		ner = "O";
+	}
+}
+
 public class Clause {
 	private ArrayList<IndexedWord> wordList;
+	private ArrayList<subNerClause> nerClauseList;
 	
 	public Clause() {
-		wordList = new ArrayList<IndexedWord>();
+		this.wordList = new ArrayList<IndexedWord>();
+		this.nerClauseList = new ArrayList<subNerClause>();
 	}
 	
 	public Clause(Clause clause) {
 		this.wordList = clause.wordList;
+		this.nerClauseList = new ArrayList<subNerClause>();
+	}
+	
+	public void findNerClause() {
+		subNerClause newNerClause = new subNerClause();
+		for (int i = 0; i < wordList.size(); i++) {
+			if (wordList.get(i).ner().equals("DATE")
+					|| wordList.get(i).ner().equals("TIME")
+					 || wordList.get(i).ner().equals("LOCATION")) {
+				
+				if (newNerClause.ner == "O") {
+					newNerClause.start = wordList.get(i).index();
+					newNerClause.ner = wordList.get(i).ner();
+				} 
+				
+				if (wordList.get(i).index() > newNerClause.end) {
+					newNerClause.end = wordList.get(i).index();	
+				}
+				
+				if (i+1 >= wordList.size()) {
+					nerClauseList.add(newNerClause);
+					newNerClause = new subNerClause();
+				} else {
+					if (wordList.get(i+1).ner() == "O") {
+						nerClauseList.add(newNerClause);
+						newNerClause = new subNerClause();
+					}
+				}
+				
+			}
+		}
+	}
+	
+	public IndexedWord get(Integer index) {
+		return wordList.get(index);
+	}
+	
+	public void remove(Integer index) {
+		wordList.remove(index);
 	}
 	
 	public boolean equals(Clause clause) {
@@ -56,6 +108,7 @@ public class Clause {
 			System.out.println(wordList.get(i).tag());
 			System.out.println(wordList.get(i).index());
 			System.out.println(wordList.get(i).word() + " ");
+			System.out.println(wordList.get(i).ner() + " ");
 
 		}
 //		System.out.print("\n");
@@ -67,12 +120,5 @@ public class Clause {
 		}
 		System.out.println();
 	}
-	
-	public IndexedWord get(Integer index) {
-		return wordList.get(index);
-	}
-	
-	public void remove(Integer index) {
-		wordList.remove(index);
-	}
 }
+
