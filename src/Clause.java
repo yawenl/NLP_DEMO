2b6afa9f8bd12b88f155d834f178f5e8ce971734
qingzhouhe <qingzhou@umich.edu>
2015-10-29 18:15:50 -0500

    Update Clause.java

--- src/Clause.java
+++ src/Clause.java
@@ -24,14 +24,17 @@ public class Clause {
 	public Clause(Clause clause) {
 		this.wordList = clause.wordList;
 		this.nerClauseList = new ArrayList<subNerClause>();
+		for (subNerClause NerClause : clause.nerClauseList()) {
+			this.nerClauseList.add(NerClause);
+		}
 	}
 	
 	public void findNerClause() {
 		subNerClause newNerClause = new subNerClause();
 		for (int i = 0; i < wordList.size(); i++) {
-			System.out.println("wordList.get(i).ner() " + wordList.get(i).ner());
-			System.out.println("wordList.get(i).word() " + wordList.get(i).word());
-			System.out.println("wordList.get(i).tag() " + wordList.get(i).tag());
+//			System.out.println("wordList.get(i).ner() " + wordList.get(i).ner());
+//			System.out.println("wordList.get(i).word() " + wordList.get(i).word());
+//			System.out.println("wordList.get(i).tag() " + wordList.get(i).tag());
 			if (wordList.get(i).ner().equals("DATE")
 					|| wordList.get(i).ner().equals("TIME")
 					 || wordList.get(i).ner().equals("LOCATION")) {
